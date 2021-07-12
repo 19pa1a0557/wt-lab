ce7a3dc33bdc85101620f3c6ad52ee86654643b2
19pa1a0557 <61247155+19pa1a0557@users.noreply.github.com>
2021-07-12 12:45:45 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="registration";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+    die("connection failed:".mysqli_connect_error());}
+else{
+    echo "sucess ";
+} 
+?>
